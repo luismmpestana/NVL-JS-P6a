a37370db987ae5d7a7024d4f75df07b4cf74a33b
Luis Miguel <luismolinapestana@gmail.com>
2020-02-19 15:57:45 +0100

    Variables

---
+++ js/java.js
@@ -0,0 +1,9 @@
+var Nombre= "Luis Miguel Molina Pestana";
+
+var Edad= 29;
+
+var DiasDeLaSemana=["Lunes","Martes","Miercoles","Jueves","Viernes","Sábado","Domingo"]
+
+var Gravedad=[9.807, "m/s²"]
+
+var NumerosImpares= [1, 3, 5];
